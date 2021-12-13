2df3fbdf94d3b2a4d64427e546c7f4502c88cae5
Александр Немеров <nemerov_a@mail.ru>
2021-12-14 02:56:37 +0800

    gotovo

--- LabW_7_Base_progs/Source.cpp
+++ LabW_7_Base_progs/Source.cpp
@@ -10,32 +10,34 @@
 
 using namespace std;
 
-void Ff(float *arr, float *result) {
-	int c(0);
-	double summ(0), gh(0);
+void Ff(float arr[], int *&num) {
+	//int c(0);
+	float summ(0), gh(0);
+	float result;
+	num = new int(0);
 
 	srand(time(NULL));
 
 	for (int i = 0; i < N; i++) {
-		*(arr+i) = rand() % 100;
+		*(arr + i) = rand() % 100;
 		summ += *(arr + i);
 		cout << *(arr + i) << " ";
+		result = summ;
 	}
 
 	summ /= N;
-	*result = *arr;
 
 	for (int i = 0; i < N; i++) {
 
 		gh = abs(*(arr + i) - summ);
 
-		if (*result > gh) {
-			*result = gh;
-			c = i;
+		if (result > gh) {
+			result = gh;
+			*num = i;
 		}
 	}
 	cout << endl << "ср. знач массива: " << summ
-		<< "\tразность: " << *result << "\tномер эл-та: " << c << endl;
+		<< "\tразность: " << result << "\tномер эл-та: " << *num << endl;
 	
 }
 
@@ -54,14 +56,15 @@ int main() {
 	ближе всех по значению к среднему арифметическому элементов 
 	массива и сохраняет его в переменной-аргументе.*/
 	
-	float arr[N]{ 0 };
-	float result(0), res(0);
+	float arr[N];
+	int* num(0);
+	char res(0);
 	
-	float* yk1 = &arr[N];
-	float* yk2 = &result;
+	//float* yk1 = &arr[N];
+	//float* yk2 = &result;
 	
-	Ff(yk1, yk2);
+	Ff(arr, num);
 
-	cout << endl << result << endl<< endl;
+	cout << endl << *num << endl << endl;
 	system("pause");
 }
